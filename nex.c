62e22b9c52d6f7a76b1f005dfe0ed3164f12e7b5
oussama <admaneo99@gmail.com>
2025-06-16 12:40:13 +0100

    test if the branches work well

--- nex.c
+++ nex.c
@@ -6,5 +6,7 @@ int main() {
 printf("hello world ") ; 
 printf("i like nesma , i mean i guess ") ; 
 printf(" i guess that no one noticed but am actually learning to use git and github ") ; 
+printf("now i added this one form another branch named ous") ; 
+
     return 0 ; 
 }
