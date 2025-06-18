5a530814665a827273c1d4b3004e599ba562899e
oussama <admaneo99@gmail.com>
2025-06-18 16:03:29 +0100

    new committt

--- nex.c
+++ nex.c
@@ -4,9 +4,6 @@
 int main() {
 
 printf("hello world ") ; 
-printf("i like nesma , i mean i guess ") ; 
-printf(" i guess that no one noticed but am actually learning to use git and github ") ; 
-printf("still learning and today i will finish those shits here ") ; 
-printf("new coommm") ; 
-    return 0 ; 
+
+return 0 ; 
 }
